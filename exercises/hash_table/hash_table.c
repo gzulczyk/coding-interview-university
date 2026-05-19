6f744ee4d91e66bdfc477b89578b46fb5bebaa85
gzulczyk <ginttermail@gmail.com>
2026-05-19 22:11:47 +0200

    feat(progress/hash_table): implement create_table helper func

--- exercises/hash_table/hash_table.c
+++ exercises/hash_table/hash_table.c
@@ -14,8 +14,33 @@ typedef struct hash_table {
 int hash(const char* k, int m) {
     int result = 0;
     for(int i=0; k[i] != '\0'; i++) {
-        result = result * 31 + k[i]; // 31 is used because we wanna spread these ASCII numbers. That 31 prime number gonna matter char order. It returns diff value when you paste a set of chars like 'abc' vs 'bca'.
+        result = result * 31 + k[i]; // 31 is used because we wanna spread these ASCII numbers. 
+                                     // That 31 prime number gonna matter char order. 
+                                     // It returns diff value when you paste a set of chars like 'abc' vs 'bca'.
     }
 
     return abs(result % m);
+}
+
+hash_table* create_table(int size) {
+    hash_table* new_hash_table = malloc(sizeof(hash_table));
+    
+    if (new_hash_table == 0) {
+        return NULL;
+    }
+
+    new_hash_table->data = malloc(sizeof(key_value*) * size); // Allocating an array of pointers [bucket], each one is a chain head.
+
+    if(new_hash_table->data == 0) {
+        return NULL;
+    }
+
+    for(int i=0; i<size; i++) {
+        new_hash_table->data[i] = NULL;
+    }
+
+    new_hash_table->size = size;
+
+    return new_hash_table;
+
 }
