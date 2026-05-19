0a704e52ecd92e2345eaead0cd2047cc631f8b53
gzulczyk <ginttermail@gmail.com>
2026-05-19 21:42:59 +0200

    feat(progress/hash_table): implement char hash function & define hash_table structs

--- exercises/hash_table/hash_table.c
+++ exercises/hash_table/hash_table.c
@@ -1,5 +1,21 @@
 #include <stdlib.h>
 
-int hash(int k, int m) {
-    return k % m;
+typedef struct key_value {
+    char* key;
+    char* value;
+    struct key_value* next;
+} key_value;
+
+typedef struct hash_table {
+    key_value** data; // It points to a list of array. One * points to a first element in the array, and key_value itself to a linked list node. 
+    int size;
+} hash_table;
+
+int hash(const char* k, int m) {
+    int result = 0;
+    for(int i=0; k[i] != '\0'; i++) {
+        result = result * 31 + k[i]; // 31 is used because we wanna spread these ASCII numbers. That 31 prime number gonna matter char order. It returns diff value when you paste a set of chars like 'abc' vs 'bca'.
+    }
+
+    return abs(result % m);
 }
